1bade46a69c352b280cc5e532fcdd06bf8ed76b4
nguyentiend199 <38046125+nguyentiend199@users.noreply.github.com>
2018-05-10 16:22:27 +0700

    2 form nhanvien và thống kê

---
+++ nhanvien.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace Quản_lý_bán_hàng
+{
+    partial class formNhanvien
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtDiachi = new System.Windows.Forms.TextBox();
+            this.txtLuong = new System.Windows.Forms.TextBox();
+            this.lbldiachi = new System.Windows.Forms.Label();
+            this.lblluong = new System.Windows.Forms.Label();
+            this.txtSDT = new System.Windows.Forms.TextBox();
+            this.txtTenNV = new System.Windows.Forms.TextBox();
+            this.txtmaNV = new System.Windows.Forms.TextBox();
+            this.lblsdt = new System.Windows.Forms.Label();
+            this.lbltennv = new System.Windows.Forms.Label();
+            this.lblmanv = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnthem = new System.Windows.Forms.Button();
+            this.btnsua = new System.Windows.Forms.Button();
+            this.btnxoa = new System.Windows.Forms.Button();
+            this.btnthoat = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtDiachi);
+            this.groupBox1.Controls.Add(this.txtLuong);
+            this.groupBox1.Controls.Add(this.lbldiachi);
+            this.groupBox1.Controls.Add(this.lblluong);
+            this.groupBox1.Controls.Add(this.txtSDT);
+            this.groupBox1.Controls.Add(this.txtTenNV);
+            this.groupBox1.Controls.Add(this.txtmaNV);
+            this.groupBox1.Controls.Add(this.lblsdt);
+            this.groupBox1.Controls.Add(this.lbltennv);
+            this.groupBox1.Controls.Add(this.lblmanv);
+            this.groupBox1.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(633, 134);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thông tin nhân viên";
+            // 
+            // txtDiachi
+            // 
+            this.txtDiachi.Location = new System.Drawing.Point(429, 63);
+            this.txtDiachi.Name = "txtDiachi";
+            this.txtDiachi.Size = new System.Drawing.Size(170, 22);
+            this.txtDiachi.TabIndex = 11;
+            // 
+            // txtLuong
+            // 
+            this.txtLuong.Location = new System.Drawing.Point(429, 29);
+            this.txtLuong.Name = "txtLuong";
+            this.txtLuong.Size = new System.Drawing.Size(170, 22);
+            this.txtLuong.TabIndex = 10;
+            // 
+            // lbldiachi
+            // 
+            this.lbldiachi.AutoSize = true;
+            this.lbldiachi.Location = new System.Drawing.Point(339, 63);
+            this.lbldiachi.Name = "lbldiachi";
+            this.lbldiachi.Size = new System.Drawing.Size(44, 15);
+            this.lbldiachi.TabIndex = 8;
+            this.lbldiachi.Text = "Địa chỉ";
+            // 
+            // lblluong
+            // 
+            this.lblluong.AutoSize = true;
+            this.lblluong.Location = new System.Drawing.Point(341, 32);
+            this.lblluong.Name = "lblluong";
+            this.lblluong.Size = new System.Drawing.Size(42, 15);
+            this.lblluong.TabIndex = 7;
+            this.lblluong.Text = "Lương";
+            // 
+            // txtSDT
+            // 
+            this.txtSDT.Location = new System.Drawing.Point(116, 94);
+            this.txtSDT.Name = "txtSDT";
+            this.txtSDT.Size = new System.Drawing.Size(179, 22);
+            this.txtSDT.TabIndex = 6;
+            // 
+            // txtTenNV
+            // 
+            this.txtTenNV.Location = new System.Drawing.Point(116, 56);
+            this.txtTenNV.Name = "txtTenNV";
+            this.txtTenNV.Size = new System.Drawing.Size(179, 22);
+            this.txtTenNV.TabIndex = 5;
+            // 
+            // txtmaNV
+            // 
+            this.txtmaNV.ForeColor = System.Drawing.Color.Black;
+            this.txtmaNV.Location = new System.Drawing.Point(116, 25);
+            this.txtmaNV.Name = "txtmaNV";
+            this.txtmaNV.Size = new System.Drawing.Size(179, 22);
+            this.txtmaNV.TabIndex = 4;
+            // 
+            // lblsdt
+            // 
+            this.lblsdt.AutoSize = true;
+            this.lblsdt.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lblsdt.Location = new System.Drawing.Point(20, 97);
+            this.lblsdt.Name = "lblsdt";
+            this.lblsdt.Size = new System.Drawing.Size(77, 15);
+            this.lblsdt.TabIndex = 2;
+            this.lblsdt.Text = "Số điện thoại";
+            // 
+            // lbltennv
+            // 
+            this.lbltennv.AutoSize = true;
+            this.lbltennv.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lbltennv.Location = new System.Drawing.Point(20, 59);
+            this.lbltennv.Name = "lbltennv";
+            this.lbltennv.Size = new System.Drawing.Size(48, 15);
+            this.lbltennv.TabIndex = 1;
+            this.lbltennv.Text = "Tên NV";
+            // 
+            // lblmanv
+            // 
+            this.lblmanv.AutoSize = true;
+            this.lblmanv.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lblmanv.Location = new System.Drawing.Point(20, 27);
+            this.lblmanv.Name = "lblmanv";
+            this.lblmanv.Size = new System.Drawing.Size(45, 15);
+            this.lblmanv.TabIndex = 0;
+            this.lblmanv.Text = "Mã NV";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dataGridView1);
+            this.groupBox2.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.groupBox2.Location = new System.Drawing.Point(12, 216);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(612, 272);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Danh sách nhân viên";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(-34, 21);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(633, 245);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.RowEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_RowEnter);
+            // 
+            // btnthem
+            // 
+            this.btnthem.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnthem.Location = new System.Drawing.Point(102, 173);
+            this.btnthem.Name = "btnthem";
+            this.btnthem.Size = new System.Drawing.Size(75, 23);
+            this.btnthem.TabIndex = 2;
+            this.btnthem.Text = "Thêm";
+            this.btnthem.UseVisualStyleBackColor = true;
+            this.btnthem.Click += new System.EventHandler(this.btnthem_Click);
+            // 
+            // btnsua
+            // 
+            this.btnsua.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnsua.Location = new System.Drawing.Point(232, 173);
+            this.btnsua.Name = "btnsua";
+            this.btnsua.Size = new System.Drawing.Size(75, 23);
+            this.btnsua.TabIndex = 3;
+            this.btnsua.Text = "Sửa";
+            this.btnsua.UseVisualStyleBackColor = true;
+            this.btnsua.Click += new System.EventHandler(this.btnsua_Click);
+            // 
+            // btnxoa
+            // 
+            this.btnxoa.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnxoa.Location = new System.Drawing.Point(366, 173);
+            this.btnxoa.Name = "btnxoa";
+            this.btnxoa.Size = new System.Drawing.Size(75, 23);
+            this.btnxoa.TabIndex = 4;
+            this.btnxoa.Text = "Xóa";
+            this.btnxoa.UseVisualStyleBackColor = true;
+            this.btnxoa.Click += new System.EventHandler(this.btnxoa_Click);
+            // 
+            // btnthoat
+            // 
+            this.btnthoat.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnthoat.Location = new System.Drawing.Point(508, 173);
+            this.btnthoat.Name = "btnthoat";
+            this.btnthoat.Size = new System.Drawing.Size(75, 23);
+            this.btnthoat.TabIndex = 5;
+            this.btnthoat.Text = "Thoát";
+            this.btnthoat.UseVisualStyleBackColor = true;
+            this.btnthoat.Click += new System.EventHandler(this.btnthoat_Click);
+            // 
+            // formNhanvien
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(663, 500);
+            this.Controls.Add(this.btnthoat);
+            this.Controls.Add(this.btnxoa);
+            this.Controls.Add(this.btnsua);
+            this.Controls.Add(this.btnthem);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "formNhanvien";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Nhân viên";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.fromNhanvien_FormClosing);
+            this.Load += new System.EventHandler(this.fromNhanvien_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtSDT;
+        private System.Windows.Forms.TextBox txtTenNV;
+        private System.Windows.Forms.TextBox txtmaNV;
+        private System.Windows.Forms.Label lblsdt;
+        private System.Windows.Forms.Label lbltennv;
+        private System.Windows.Forms.Label lblmanv;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtDiachi;
+        private System.Windows.Forms.TextBox txtLuong;
+        private System.Windows.Forms.Label lbldiachi;
+        private System.Windows.Forms.Label lblluong;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnthem;
+        private System.Windows.Forms.Button btnsua;
+        private System.Windows.Forms.Button btnxoa;
+        private System.Windows.Forms.Button btnthoat;
+    }
+}
